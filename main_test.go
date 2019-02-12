3aea9304fa3ce9f6cbbb40a255a77d9fe76932ca
Carsten Thiel <carsten.thiel@cessda.eu>
2019-02-12 14:00:12 +0100

    Test healthcheck response

--- main_test.go
+++ main_test.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+  "encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/gin-gonic/gin"
 )
 
-func TestPingRoute(t *testing.T) {
+func TestRootRoute(t *testing.T) {
 	router := setupRouter()
 
 	w := httptest.NewRecorder()
@@ -18,3 +21,31 @@ func TestPingRoute(t *testing.T) {
 	assert.Equal(t, 200, w.Code)
 	assert.Equal(t, "Hello World!", w.Body.String())
 }
+
+func TestHealthcheck(t *testing.T) {
+  responsejson := gin.H{
+    "message": "Ok",
+  }
+
+  router := setupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/healthcheck", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+
+  // Convert the JSON response to a map
+  var response map[string]string
+  err := json.Unmarshal([]byte(w.Body.String()), &response)
+
+  // Grab the value & whether or not it exists
+  value, exists := response["message"]
+
+  // Make some assertions on the correctness of the response.
+  assert.Nil(t, err)
+  assert.True(t, exists)
+  assert.Equal(t, responsejson["message"], value)
+
+}
+
