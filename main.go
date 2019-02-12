3afc4fbceaf7300fedb4eb33870688952a2b7fef
Carsten Thiel <carsten.thiel@cessda.eu>
2019-02-12 13:27:55 +0100

    Logging healthcheck requests

--- main.go
+++ main.go
@@ -1,19 +1,45 @@
 package main
 
 import (
+  "time"
 	"net/http"
+  "github.com/sirupsen/logrus"
+  "github.com/toorop/gin-logrus"
 	"github.com/gin-gonic/gin"
 )
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
 
-	r.GET("/", func(c *gin.Context) {
+  log := logrus.New()
+  log.SetLevel(logrus.DebugLevel)
+  //log.SetFormatter(&log.JSONFormatter{})
+  r.Use(ginlogrus.Logger(log), gin.Recovery())
+
+  r.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello World!")
 	})
 
   r.GET("/healthcheck", func(c *gin.Context) {
+
+    start := time.Now()
+
+    log.WithFields(logrus.Fields{
+      "method": "GET",
+      "endpoint": "healthcheck",
+    }).Debug("request received")
+
     c.JSON(http.StatusOK, gin.H{"message": "Ok"})
+
+    elapsed := time.Since(start)
+
+    log.WithFields(logrus.Fields{
+      "method": "GET",
+      "status": http.StatusOK,
+      "responsetime": elapsed,
+      "endpoint": "healthcheck",
+    }).Debug("request answered")
+
 	})
 
 	return r
