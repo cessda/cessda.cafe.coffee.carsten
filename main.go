4a6cf0455085db2cbb95c61fe02a21e1b52818ed
Carsten Thiel <carsten.thiel@cessda.eu>
2019-02-24 15:54:38 +0100

    Adhere to gofmt style

--- main.go
+++ main.go
@@ -1,140 +1,137 @@
 package main
 
 import (
-  "time"
-  "net/http"
-  "github.com/sirupsen/logrus"
-  "github.com/gin-gonic/gin"
-  "github.com/atarantini/ginrequestid"
+	"github.com/atarantini/ginrequestid"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"net/http"
+	"time"
 )
 
 type coffeeOrder struct {
-  Type      string `json:"type"`
+	Type string `json:"type"`
 }
 
-
 func myRequestLogger(log *logrus.Logger) gin.HandlerFunc {
-  return func(c *gin.Context) {
+	return func(c *gin.Context) {
 
-    path := c.Request.URL.Path
+		path := c.Request.URL.Path
 
-    start := time.Now()
+		start := time.Now()
 
-    log.WithFields(logrus.Fields{
-      "method": "GET",
-      "requestId": c.MustGet("RequestId"),
-      "endpoint": path,
-    }).Debug("request received")
+		log.WithFields(logrus.Fields{
+			"method":    "GET",
+			"requestId": c.MustGet("RequestId"),
+			"endpoint":  path,
+		}).Debug("request received")
 
+		c.Next()
 
-    c.Next()
+		elapsed := time.Since(start)
 
-    elapsed := time.Since(start)
+		log.WithFields(logrus.Fields{
+			"method":       "GET",
+			"requestId":    c.MustGet("RequestId"),
+			"status":       http.StatusOK,
+			"responsetime": elapsed,
+			"endpoint":     path,
+		}).Debug("request answered")
 
-    log.WithFields(logrus.Fields{
-      "method": "GET",
-      "requestId": c.MustGet("RequestId"),
-      "status": http.StatusOK,
-      "responsetime": elapsed,
-      "endpoint": path,
-    }).Debug("request answered")
-
-  }
+	}
 }
 
 func setupRouter() *gin.Engine {
-  r := gin.New()
+	r := gin.New()
 
-  r.Use(ginrequestid.RequestId())
+	r.Use(ginrequestid.RequestId())
 
-  log := logrus.New()
-  log.SetLevel(logrus.DebugLevel)
+	log := logrus.New()
+	log.SetLevel(logrus.DebugLevel)
 
-  r.Use(myRequestLogger(log), gin.Recovery())
+	r.Use(myRequestLogger(log), gin.Recovery())
 
-  r.GET("/", func(c *gin.Context) {
-    c.String(http.StatusOK, "Hello World!")
-  })
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello World!")
+	})
 
-  r.GET("/healthcheck", func(c *gin.Context) {
+	r.GET("/healthcheck", func(c *gin.Context) {
 
-    c.JSON(http.StatusOK, gin.H{"message": "Ok"})
+		c.JSON(http.StatusOK, gin.H{"message": "Ok"})
 
-  })
+	})
 
-  r.GET("/status", func(c *gin.Context) {
+	r.GET("/status", func(c *gin.Context) {
 
-    systemStatusCode, systemStatusMessage := systemStatus()
+		systemStatusCode, systemStatusMessage := systemStatus()
 
-    c.JSON(systemStatusCode, gin.H{"message": systemStatusMessage})
+		c.JSON(systemStatusCode, gin.H{"message": systemStatusMessage})
 
-  })
+	})
 
-  r.GET("/order-history", func(c *gin.Context) {
+	r.GET("/order-history", func(c *gin.Context) {
 
-    c.JSON(http.StatusOK, getAllOrders())
+		c.JSON(http.StatusOK, getAllOrders())
 
-  })
+	})
 
-  r.GET("/order-history/:ID", func(c *gin.Context) {
-    ID := c.Param("ID")
-    order, success := getOrderbyID(ID)
+	r.GET("/order-history/:ID", func(c *gin.Context) {
+		ID := c.Param("ID")
+		order, success := getOrderbyID(ID)
 
-    if ! success {
-      c.JSON(http.StatusNotFound, gin.H{"message": "Order not found!"})
-    } else {
-      c.JSON(http.StatusOK, order)
-    }
-  })
+		if !success {
+			c.JSON(http.StatusNotFound, gin.H{"message": "Order not found!"})
+		} else {
+			c.JSON(http.StatusOK, order)
+		}
+	})
 
-  r.POST("/order-coffee", func(c *gin.Context) {
-    var incomingOrder coffeeOrder
-    c.BindJSON(&incomingOrder)
+	r.POST("/order-coffee", func(c *gin.Context) {
+		var incomingOrder coffeeOrder
+		c.BindJSON(&incomingOrder)
 
-    result, success, systemStatus := newOrder(incomingOrder.Type)
+		result, success, systemStatus := newOrder(incomingOrder.Type)
 
-    if !success {
-      log.WithFields(logrus.Fields{
-        "requestId": c.MustGet("RequestId"),
-      }).Debug("Error placing order")
-      c.JSON(http.StatusBadRequest, gin.H{"message": systemStatus})
-    } else {
-      log.WithFields(logrus.Fields{
-        "requestId": c.MustGet("RequestId"),
-        "type": result.Type,
-        "readyAt": result.ReadyAt,
-      }).Debug("Order placed")
-      c.JSON(http.StatusOK, result)
-    }
+		if !success {
+			log.WithFields(logrus.Fields{
+				"requestId": c.MustGet("RequestId"),
+			}).Debug("Error placing order")
+			c.JSON(http.StatusBadRequest, gin.H{"message": systemStatus})
+		} else {
+			log.WithFields(logrus.Fields{
+				"requestId": c.MustGet("RequestId"),
+				"type":      result.Type,
+				"readyAt":   result.ReadyAt,
+			}).Debug("Order placed")
+			c.JSON(http.StatusOK, result)
+		}
 
-  })
+	})
 
-  r.GET("/retrieve-coffee/:ID", func(c *gin.Context) {
-    ID := c.Param("ID")
-    order, success := retrieveOrder(ID)
-      log.WithFields(logrus.Fields{
-        "requestId": c.MustGet("RequestId"),
-        "success": success,
-      }).Debug("Request to retrieve Coffee")
-    if ! success {
-      log.WithFields(logrus.Fields{
-        "requestId": c.MustGet("RequestId"),
-        "success": success,
-        "order-id": ID,
-      }).Debug("Failed to retrieve Coffee")
-      c.JSON(http.StatusBadRequest, gin.H{"message": "Nope!"})
-    } else {
-      c.JSON(http.StatusOK, order)
-    }
-  })
+	r.GET("/retrieve-coffee/:ID", func(c *gin.Context) {
+		ID := c.Param("ID")
+		order, success := retrieveOrder(ID)
+		log.WithFields(logrus.Fields{
+			"requestId": c.MustGet("RequestId"),
+			"success":   success,
+		}).Debug("Request to retrieve Coffee")
+		if !success {
+			log.WithFields(logrus.Fields{
+				"requestId": c.MustGet("RequestId"),
+				"success":   success,
+				"order-id":  ID,
+			}).Debug("Failed to retrieve Coffee")
+			c.JSON(http.StatusBadRequest, gin.H{"message": "Nope!"})
+		} else {
+			c.JSON(http.StatusOK, order)
+		}
+	})
 
-
-  return r
+	return r
 }
 
 func main() {
 
-  r := setupRouter()
-  r.Run(":1337")
+	r := setupRouter()
+	r.Run(":1337")
 
 }
--- main_test.go
+++ main_test.go
@@ -1,58 +1,57 @@
 package main
 
 import (
-  "encoding/json"
-  "net/http"
-  "net/http/httptest"
-  "testing"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
-  "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 
-  "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func TestRootRoute(t *testing.T) {
-  router := setupRouter()
+	router := setupRouter()
 
-  w := httptest.NewRecorder()
-  req, _ := http.NewRequest("GET", "/", nil)
-  router.ServeHTTP(w, req)
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/", nil)
+	router.ServeHTTP(w, req)
 
-  assert.Equal(t, 200, w.Code)
-  assert.Equal(t, "Hello World!", w.Body.String())
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "Hello World!", w.Body.String())
 }
 
 func TestHealthcheck(t *testing.T) {
-  var requestUUID string
-  requestUUID = "11122333-aaaa-bbbb-cccc-444555666777"
+	var requestUUID string
+	requestUUID = "11122333-aaaa-bbbb-cccc-444555666777"
 
-  responsejson := gin.H{
-    "message": "Ok",
-  }
+	responsejson := gin.H{
+		"message": "Ok",
+	}
 
-  router := setupRouter()
+	router := setupRouter()
 
-  w := httptest.NewRecorder()
-  req, _ := http.NewRequest("GET", "/healthcheck", nil)
-  req.Header.Add("X-Request-Id", requestUUID)
-  router.ServeHTTP(w, req)
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/healthcheck", nil)
+	req.Header.Add("X-Request-Id", requestUUID)
+	router.ServeHTTP(w, req)
 
-  assert.Equal(t, 200, w.Code)
+	assert.Equal(t, 200, w.Code)
 
-  // Convert the JSON response to a map
-  var response map[string]string
-  err := json.Unmarshal([]byte(w.Body.String()), &response)
+	// Convert the JSON response to a map
+	var response map[string]string
+	err := json.Unmarshal([]byte(w.Body.String()), &response)
 
-  // Grab the value & whether or not it exists
-  value, exists := response["message"]
+	// Grab the value & whether or not it exists
+	value, exists := response["message"]
 
-  // Make some assertions on the correctness of the response.
-  assert.Nil(t, err)
-  assert.True(t, exists)
-  assert.Equal(t, responsejson["message"], value)
+	// Make some assertions on the correctness of the response.
+	assert.Nil(t, err)
+	assert.True(t, exists)
+	assert.Equal(t, responsejson["message"], value)
 
-  // check request header
-  assert.Contains(t, req.Header["X-Request-Id"], requestUUID)
+	// check request header
+	assert.Contains(t, req.Header["X-Request-Id"], requestUUID)
 
 }
-
