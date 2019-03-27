646c83b6c978d2f920ecc3f16e74d17a35d7eeab
Carsten Thiel <cat@schildwaechter.io>
2019-03-27 14:25:46 +0100

    Improve sonar config

--- run-tests.sh
+++ run-tests.sh
@@ -5,6 +5,7 @@ go get -u golang.org/x/lint/golint
 go get -u github.com/jstemmer/go-junit-report
 go get -u github.com/kardianos/govendor
 govendor sync
-golint -set_exit_status $(go list ./... | grep -v /vendor/)
-go vet $(go list ./... | grep -v /vendor/)
+golint -set_exit_status $(go list ./... | grep -v /vendor/) > golint-report.out
+go vet $(go list ./... | grep -v /vendor/) 2> govet-report.out
 go test -coverprofile=coverage.out -v 2>&1 | go-junit-report > report.xml
+
