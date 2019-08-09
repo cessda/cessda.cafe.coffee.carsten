fe47ff0a6ff1ddc6ba9c4edaef9beeca43da8243
Carsten Thiel <carsten.thiel@cessda.eu>
2019-08-09 13:56:19 +0200

    Adding APACHE-2 License

--- main_test.go
+++ main_test.go
@@ -1,3 +1,17 @@
+// Carsten's CESSDA CAFE Coffee Machine
+// Copyright CESSDA-ERIC 2019
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
@@ -11,6 +25,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Validate the response for root path
 func TestRootRoute(t *testing.T) {
 	router := setupRouter()
 
@@ -22,6 +37,7 @@ func TestRootRoute(t *testing.T) {
 	assert.Equal(t, "Hello World!", w.Body.String())
 }
 
+// Validate HealthCeck path
 func TestHealthcheck(t *testing.T) {
 	var requestUUID string
 	requestUUID = "11122333-aaaa-bbbb-cccc-444555666777"
